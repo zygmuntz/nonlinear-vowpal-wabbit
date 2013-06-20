dfba21defe72b9273eddfd4c2b1888631ee68630
zygmuntz <zajac.zygmunt@gmail.com>
2013-06-20 22:36:13 +0200

    initial commit - kin8nm data and some code

---
+++ README.md
@@ -0,0 +1,6 @@
+Go non-linear with Vowpal Wabbit
+================================
+
+See [http://fastml.com/go-non-linear-with-vowpal-wabbit/](http://fastml.com/go-non-linear-with-vowpal-wabbit/) for description.
+
+	
---
+++ kin8nm/rmse.py
@@ -0,0 +1,29 @@
+'compute RMSE from VW validation and predictions file'
+
+import sys, csv, math
+
+test_file = sys.argv[1]
+predictions_file = sys.argv[2]
+
+test_reader = csv.reader( open( test_file ), delimiter = " " )
+p_reader = csv.reader( open( predictions_file ), delimiter = "\n" )
+
+squared_diffs = []
+n = 0
+
+for p_line in p_reader:
+	test_line = test_reader.next()
+	n += 1
+	
+	p = float( p_line[0] )
+	y = float( test_line[0] )
+	
+	squared_diff = math.pow( y - p, 2 )
+	squared_diffs.append( squared_diff )
+
+squared_diffs = sum( squared_diffs )
+MSE = squared_diffs / n
+RMSE = math.sqrt( MSE )
+
+print "RMSE: %s" % ( RMSE )
+print
